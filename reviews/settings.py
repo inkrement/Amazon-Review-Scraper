48836c34a15db1e623426cbe6e5d821bba69f5f5
Tharun <tgomudur@asu.edu>
2016-01-25 21:25:37 -0700

    Update settings.py

--- reviews/settings.py
+++ reviews/settings.py
@@ -6,9 +6,11 @@ SPIDER_MODULES = ['reviews.spiders']
 NEWSPIDER_MODULE = 'reviews.spiders'
 DEFAULT_ITEM_CLASS = 'reviews.items.ReviewsItem'
 
-ITEM_PIPELINES = [
-     'reviews.pipelines.MySQLStorePipeline',
-]
+# If you want to pipeline the output of the spider to MySQLDb server, uncomment the following lines
+# and enter credentials of the server in reviews/pipelines.py
+# ITEM_PIPELINES = [
+#      'reviews.pipelines.MySQLStorePipeline',
+# ]
 
 DOWNLOAD_HANDLERS = {
   's3': None,
